af6bec9d4d778758fdf1bb42363895f6f92a931c
Unknown <tyfurness@gmail.com>
2016-10-04 15:29:40 -0400

    Chapter 3/4

---
+++ Chapter3/Chapter3.cpp
@@ -0,0 +1,35 @@
+// Chapter3.cpp : Defines the entry point for the console application.
+//
+
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+/// 3.2 work
+//bool isEven(int x)
+//{
+//	return (x % 2) == 0;
+//}
+//
+//int main()
+//{
+//	int x;
+//	cout << "Enter an integer and i'll tell you if it's even or not." << endl;
+//	cin >> x;
+//
+//	if (isEven(x))
+//	{
+//		cout << x << " is even\n";
+//	}
+//	else
+//
+//	{
+//		cout << x << " is odd\n";
+//	}
+//	return 0;
+//}
+
+/// ---------------------------
+
+
