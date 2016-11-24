7b5032c35933a9c2f206bc8af9b2cd72ac9f85a9
Unknown <tyfurness@gmail.com>
2016-11-23 22:36:05 -0500

    Worked on chapter 4 stuff.

--- Chapter4/Chapter4.cpp
+++ Chapter4/Chapter4.cpp
@@ -2,10 +2,28 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <string>
+#include <cstdio>
 
 int main()
 {
-    return 0;
+	std::cout << "Enter your full name: ";
+	std::string name;
+	std::getline(std::cin, name); // read a full line of text into name
+
+	std::cout << "Enter your age: ";
+	int age; // age needs to be an integer, not a string, so we can do math with it
+	std::cin >> age;
+
+	std::cin.ignore(32767, '\n');
+	int letters = name.length(); // get number of letters in name (including spaces)
+	double agePerLetter = static_cast<double>(age) / letters; // static cast age to double to avoid integer division
+	std::cout << "You've lived " << agePerLetter << " years for each letter in your name.\n";
+
+	std::getchar();
+
+	return 0;
 }
+
 
